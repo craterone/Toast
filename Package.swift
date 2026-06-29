98c53385b0414adfc9dd3f842b51d87e64e31edd
craterone <47512039+craterone@users.noreply.github.com>
2026-06-29 14:48:42 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "Toast",
     platforms: [
-        .macOS(.v14)
+        .macOS(.v14),
+        iOS(.v18)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
